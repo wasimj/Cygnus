062552a1c06e0584a557355484159e749a6eb0be
Wasim <w@juned.net>
2023-07-16 22:31:36 +0100

    update

--- src/components/Menu/MenuOptions/MenuOptions.tsx
+++ src/components/Menu/MenuOptions/MenuOptions.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useStore from '@store/store';
 
 import Api from './Api';
-import Me from './Me';
+// import Me from './Me';
 import AboutMenu from '@components/AboutMenu';
 import ImportExportChat from '@components/ImportExportChat';
 import SettingsMenu from '@components/SettingsMenu';
